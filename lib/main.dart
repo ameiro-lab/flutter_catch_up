460bfad75b58b7bd4fdee11eb6c2462837def6d9
ameiro-lab <ameiro.lab@gmail.com>
2025-06-04 18:18:25 +0900

    Hive パッケージの導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catch_up/models/todo.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // main() 内で非同期処理（awaitを使う処理）がある場合、必須の記述
+
+  // モバイル端末内のデータ保存用ディレクトリを取得する =>ここにHiveのデータファイルを置くことで、ユーザーの端末内にデータが永続化される
+  final appDocDir = await getApplicationDocumentsDirectory();
+
+  await Hive.initFlutter(appDocDir.path); // Hiveを初期化
+  Hive.registerAdapter(TodoAdapter());    // HiveにTodoクラスのアダプターを登録 =>todo.g.dart で自動生成されたクラスを指定する。これによりHiveがTodoクラスのデータを保存・読み込みできるようになる
   runApp(const MyTodo());
 }
--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,7 +1,15 @@
-/// タスクDTOクラス
-class Todo {
-  String title;       // タスクのタイトル
-  bool isDone;        // タスクが完了しているかどうか
+import 'package:hive/hive.dart';
+
+part 'todo.g.dart';
+
+@HiveType(typeId: 0)  // Hiveで保存されるデータ型であることを示す。idはアプリ上で一意のキー
+class Todo extends HiveObject {
+
+  @HiveField(0) // Hiveで保存されるデータであることを示す。指定した番号は絶対に変えないこと
+  String title;
+
+  @HiveField(1)
+  bool isDone;
 
   // コンストラクタ：タイトルを必須とし、完了状態は初期値false
   Todo({
